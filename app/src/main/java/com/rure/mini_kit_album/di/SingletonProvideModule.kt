8224c9a5319ae983b7be363cd8ca1c19f2553ea6
rure00 <asnjkd@naver.com>
2026-01-01 16:05:40 +0900

    add: hilt di logic about data layer

--- app/src/main/java/com/rure/mini_kit_album/di/SingletonProvideModule.kt
+++ app/src/main/java/com/rure/mini_kit_album/di/SingletonProvideModule.kt
@@ -1,6 +1,9 @@
 package com.rure.mini_kit_album.di
 
 
+import android.content.Context
+import androidx.room.Room
+import com.rure.data.room.MainRoomDataBase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,4 +15,21 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class SingletonProvideModule {
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): MainRoomDataBase {
+        return  Room.databaseBuilder(
+            context,
+            MainRoomDataBase::class.java,
+            "app_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideLocalDataSource(roomDataBase: MainRoomDataBase) = roomDataBase.localDataSource
+
+    @Provides
+    @Singleton
+    fun provideIoDispatcher() = Dispatchers.IO
 }
